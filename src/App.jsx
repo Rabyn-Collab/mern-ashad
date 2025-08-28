ee0d1a9af7ff5fd41db253cd591d25eacf37abd8
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-28 09:07:30 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router-dom";
-import About from "./pages/about/About.jsx";
-import Contact from "./pages/contact/Contact.jsx";
 import Home from "./pages/home/Home.jsx";
-
 import RootLayOut from "./components/RootLayOut.jsx";
 import NotFound from "./pages/not-found/NotFound.jsx";
 
@@ -18,14 +15,7 @@ export default function App() {
           element: <Home />
         },
 
-        {
-          path: 'about',
-          element: <About />
-        },
-        {
-          path: 'contact',
-          element: <Contact />
-        }
+
       ]
     },
     {
