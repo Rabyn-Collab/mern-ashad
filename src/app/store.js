240c0c0205974e9a5e4a7c109b5b02f8f0eceb1a
Rabyn-Collab <babynshrestha64@gmail.com>
2025-09-17 09:53:53 +0545

    changes

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { cocktailApi } from "../features/cocktails/cocktailApi";
 
 
 
 
 export const store = configureStore({
   reducer: {
+    cocktailApi: cocktailApi.reducer
+  },
 
-
-
-  }
+  //caching , invalidation, Polling
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([
+      cocktailApi.middleware
+    ]),
 });
--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,7 +1,27 @@
+import { Image } from "@heroui/react";
+import { useGetCocktailsQuery } from "../cocktails/cocktailApi"
 
 export default function Home() {
+
+  const { data, isLoading, error } = useGetCocktailsQuery();
+  if (isLoading) return <h1>Loading....</h1>
+  if (error) return <h1 className="text-red-500">{error.data}</h1>
+
+
   return (
-    <div>
+    <div className="grid grid-cols-4 gap-5 p-5">
+      {data && data.drinks.map((item) => {
+        return (
+          <div key={item.idDrink} className="space-y-4">
+            <h1>{item.strDrink}</h1>
+            <Image
+              isBlurred
+              src={item.strDrinkThumb}
+              alt={item.strDrink}
+            />
+          </div>
+        )
+      })}
 
     </div>
   )
