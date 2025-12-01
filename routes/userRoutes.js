4e89cb5a8ceb18ab13b7621f95b1a778b304ce2c
Rabyn-Collab <babynshrestha64@gmail.com>
2025-12-01 09:33:59 +0545

    css create push

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { notAllowed } from '../utils/notAllowed.js';
-import { loginUser, registerUser } from '../controllers/userController.js';
+import { getUser, loginUser, registerUser, updateProfile } from '../controllers/userController.js';
+import { checkUser } from '../middlewares/checkUser.js';
 
 
 
@@ -8,6 +9,7 @@ const router = express.Router();
 
 router.route('/api/users/login').post(loginUser).all(notAllowed);
 router.route('/api/users/register').post(registerUser).all(notAllowed);
+router.route('/api/users').get(checkUser, getUser).patch(checkUser, updateProfile).all(notAllowed);
 
 
 export default router;
